c0b95819d66020f9e9aa57d7529fb754aa7fa799
jegini <jeginilokesh012022@gmail.com>
2022-09-12 21:12:32 +0530

    here i write code upto json file create ,json data fetch is pending

---
+++ index.js
@@ -0,0 +1,11 @@
+//we can use json object as like java script it will not give error (here in normal js we use variable to store the entire object but in json we did not use variable concept)
+var a = {
+    "name": "jegini lokesh",
+    "age": 25,
+    "skills": ["html", "cs", "js"],
+    "address": {
+        "city": "pathagunta",
+        "state": "andhra pradesh"
+    }
+}
+console.table(a)
